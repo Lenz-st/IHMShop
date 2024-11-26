5515fba750e642c655e3a46855e511d9b42e7bcd
Lenz-st <skyzchanel@gmail.com>
2024-11-26 21:04:57 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-// Example: Simple alert for "Buy Now" button
-const buyButtons = document.querySelectorAll(".buy-now");
-
-buyButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        alert("This feature is under development!");
-    });
-});
-// JavaScript to toggle menu
-const mobileMenu = document.getElementById('mobile-menu');
-const navLinks = document.getElementById('nav-links');
-
 mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    const navLinks = document.getElementById('nav-links');
     navLinks.classList.toggle('show');
 });
